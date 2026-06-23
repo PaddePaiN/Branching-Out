c2452b655d2c4848dbc11edada77471f3fb44729
Patrick Förderer <patrick.foerderer.1989@gmail.com>
2026-06-23 13:17:11 +0200

    Implement filter selection in main function

--- filter_users.py
+++ filter_users.py
@@ -43,30 +43,24 @@ def filter_users_by_email(email):
 
 def main():
     filter_option = input(
-        "What would you like to filter by "
-        "(name, age, or email)? "
+        "What would you like to filter by? "
+        "(name, email, age): "
     ).strip().lower()
 
     if filter_option == "name":
-        name_to_search = input(
-            "Enter a name to filter users: "
-        ).strip()
-        filter_users_by_name(name_to_search)
-
-    elif filter_option == "age":
-        age_to_search = int(
-            input("Enter an age to filter users: ").strip()
-        )
-        filter_users_by_age(age_to_search)
+        name = input("Enter a name: ").strip()
+        filter_users_by_name(name)
 
     elif filter_option == "email":
-        email_to_search = input(
-            "Enter an email to filter users: "
-        ).strip()
-        filter_users_by_email(email_to_search)
+        email = input("Enter an email: ").strip()
+        filter_users_by_email(email)
+
+    elif filter_option == "age":
+        age = int(input("Enter an age: ").strip())
+        filter_users_by_age(age)
 
     else:
-        print("Filtering by that option is not yet supported.")
+        print("Invalid filter option.")
 
 
 if __name__ == "__main__":
